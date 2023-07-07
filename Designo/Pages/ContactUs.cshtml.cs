15c24a97d37e1f1702ba0dc2c053e4dcf76b7868
Ebenezer Ahurein <ebenezer.ahurein@amalitech.com>
2023-07-07 06:28:18 +0000

    link all designs

---
+++ Designo/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Designo.Pages;
+
+public class ContactUsModel : PageModel
+{
+    public Dictionary<string, Dictionary<string, string>> Locations { get; set; } = new()
+        {
+            {
+                "canada", new() {
+                     {"image", "illustration-canada.svg"},
+                     {"country", "canada"},
+                     {"link", ""},
+                }
+            },
+            {
+                "australia", new() {
+                     {"image", "illustration-australia.svg"},
+                     {"country", "australia"},
+                     {"link", ""},
+                }
+            },
+            {
+                "united", new() {
+                     {"image", "illustration-united-kingdom.svg"},
+                     {"country", "united Kingdom"},
+                     {"link", ""},
+                }
+            }
+    };
+
+    public void OnGet()
+    {
+    }
+}
